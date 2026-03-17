89c979dee052d0268bc25b6f1a3291350575bf63
ayumi-pixel <isabelehashioka@gmail.com>
2026-03-16 22:38:47 -0300

    Adicionado demais exercicios

---
+++ Printttt.py
@@ -0,0 +1,7 @@
+nome = "Ayumi"
+idade = 18
+valor = 10000000000
+
+print(nome,"de",idade,"anos tem R$ ", valor)
+print("{} de {} anos, tem R$ {}".format(nome,idade,valor))
+print(f"{nome} de {idade} anos, tem R$ {valor}")
